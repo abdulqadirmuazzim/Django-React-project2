d6300fa05ae3cb3280e755188fd3b76cddd1ba34
abdulqadir <abdulqadimuazzim@gmail.com>
2025-07-14 18:41:01 +0100

    another bug fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-import Login from './pages/login'
 import Register from './pages/register'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import ProtectedRoute from './components/protectedRoute'
+import Login from "./pages/Login"
 
 const Logout = () =>{
   localStorage.clear()
